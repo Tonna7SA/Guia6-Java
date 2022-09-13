a3cea4da1a226f1c5b35740054277904f7fbd8a3
Tonna7SA <gjbarrasa@gmail.com>
2022-09-12 23:56:38 -0300

    Decimo Commit

--- EjExtra23Guia6/src/ejextra23guia6/EjExtra23Guia6.java
+++ EjExtra23Guia6/src/ejextra23guia6/EjExtra23Guia6.java
@@ -29,7 +29,7 @@ public class EjExtra23Guia6 {
         Scanner leer = new Scanner(System.in);
         String matrizsopa[][] = new String[20][20];
         String matrizpalabra[][] = new String[5][5];
-        int aleatorio[] = {6, 10, 15, 2, 18};
+        int aleatorio[] = new int[5];
         int contador = 0;
         int i = 0;
         System.out.println("Ingrese 5 palabras para la Sopa de Letras");
@@ -53,16 +53,30 @@ public class EjExtra23Guia6 {
             i++;
             contador++;
         } while (contador < 5);
+        for (int j = 0; j < 5; j++) {
+            boolean bandera = false;
+            do {
+                aleatorio[j] = (int) (Math.random() *20);
+                for (int k = 0; k < j; k++) {
+                    if (aleatorio[j] == aleatorio[k]) {
+                        bandera = true;
+                        break;
+                    } else {
+                        bandera = false;
+                    }
+                }
+            } while (bandera == true);
+        }
         System.out.println("");
         for (int k = 0; k < matrizpalabra.length; k++) {
-            for (int l = 0; l < matrizpalabra[0].length;l++) {
+            for (int l = 0; l < matrizpalabra[0].length; l++) {
                 matrizsopa[aleatorio[k]][l] = matrizpalabra[k][l];
             }
         }
         System.out.println("----S O P A---D E---L E T R A S----");
         System.out.println(" ");
-        for (int j=0; j<matrizsopa.length;j++){
-            for (int z = 0; z < matrizsopa[0].length;z++) {
+        for (int j = 0; j < matrizsopa.length; j++) {
+            for (int z = 0; z < matrizsopa[0].length; z++) {
                 if (matrizsopa[j][z] == null) {
                     matrizsopa[j][z] = String.valueOf((int) (Math.random() * 10));
                 }
